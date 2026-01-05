4cc20498cdca628afacf06f88ab08c53e1883846
Elizaveta Galimova <egalimova@marfa-tech.com>
2026-01-05 16:35:41 +0500

    forgot about linter again

--- src/DiffTree.php
+++ src/DiffTree.php
@@ -4,58 +4,58 @@ namespace Differ\DiffTree;
 
 function build(array $data1, array $data2): array
 {
-	$keys = array_values(array_unique(array_merge(array_keys($data1), array_keys($data2))));
-	sort($keys);
+    $keys = array_values(array_unique(array_merge(array_keys($data1), array_keys($data2))));
+    sort($keys);
 
-	$result = [];
+    $result = [];
 
-	foreach ($keys as $key) {
-		$has1 = array_key_exists($key, $data1);
-		$has2 = array_key_exists($key, $data2);
+    foreach ($keys as $key) {
+        $has1 = array_key_exists($key, $data1);
+        $has2 = array_key_exists($key, $data2);
 
-		if (!$has1) {
-			$result[] = [
-				'type' => 'added',
-				'key' => $key,
-				'value' => $data2[$key],
-			];
-			continue;
-		}
+        if (!$has1) {
+            $result[] = [
+                'type' => 'added',
+                'key' => $key,
+                'value' => $data2[$key],
+            ];
+            continue;
+        }
 
-		if (!$has2) {
-			$result[] = [
-				'type' => 'removed',
-				'key' => $key,
-				'value' => $data1[$key],
-			];
-			continue;
-		}
+        if (!$has2) {
+            $result[] = [
+                'type' => 'removed',
+                'key' => $key,
+                'value' => $data1[$key],
+            ];
+            continue;
+        }
 
-		$v1 = $data1[$key];
-		$v2 = $data2[$key];
+        $v1 = $data1[$key];
+        $v2 = $data2[$key];
 
-		if (isAssocArray($v1) && isAssocArray($v2)) {
-			$result[] = [
-				'type' => 'nested',
-				'key' => $key,
-				'children' => build($v1, $v2),
-			];
-			continue;
-		}
+        if (isAssocArray($v1) && isAssocArray($v2)) {
+            $result[] = [
+                'type' => 'nested',
+                'key' => $key,
+                'children' => build($v1, $v2),
+            ];
+            continue;
+        }
 
-		$result[] = ($v1 === $v2)
-			? ['type' => 'unchanged', 'key' => $key, 'value' => $v1]
-			: ['type' => 'changed', 'key' => $key, 'oldValue' => $v1, 'newValue' => $v2];
-	}
+        $result[] = ($v1 === $v2)
+            ? ['type' => 'unchanged', 'key' => $key, 'value' => $v1]
+            : ['type' => 'changed', 'key' => $key, 'oldValue' => $v1, 'newValue' => $v2];
+    }
 
-	return $result;
+    return $result;
 }
 
 function isAssocArray(mixed $value): bool
 {
-	if (!is_array($value)) {
-		return false;
-	}
+    if (!is_array($value)) {
+        return false;
+    }
 
-	return array_keys($value) !== range(0, count($value) - 1);
+    return array_keys($value) !== range(0, count($value) - 1);
 }
